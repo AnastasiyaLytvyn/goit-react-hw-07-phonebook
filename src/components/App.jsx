de72447689f5516a345d55acc8f6a4ce305319ab
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-02-03 10:29:47 +0200

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,19 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Container, TitlePhone, TitleContacts } from './App.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoading } from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 export function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+  
   return (
     <Container>
       <TitlePhone>Phonebook</TitlePhone>
@@ -11,6 +22,7 @@ export function App() {
       <TitleContacts>Contacts</TitleContacts>
 
       <Filter />
+      {isLoading&& <h2>Request in progress...</h2>}
       <ContactList />
     </Container>
   );
